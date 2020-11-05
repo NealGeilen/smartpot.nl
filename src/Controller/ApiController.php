7146ba6064e33cd928758ffd04983a26eb6c42a6
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2020-11-05 09:22:05 +0100

    Api responses

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +13,9 @@ class ApiController extends AbstractController
     /**
      * @Route("/api", name="api")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return new JsonResponse(["Hi"]);
+        return new JsonResponse($request->request->all());
     }
 
     /**
