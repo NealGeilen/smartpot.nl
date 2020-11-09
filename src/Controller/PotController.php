0ccbaa5cb02c74526e1f07c8fc31e1f3ef5a23d6
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2020-11-09 15:40:38 +0100

    Update

--- src/Controller/PotController.php
+++ src/Controller/PotController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Pot;
 use App\Entity\User;
+use App\Form\PotType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
@@ -20,6 +23,33 @@ class PotController extends AbstractController
         return $this->render('pot/index.html.twig', [
             "Pots" => $user->getPot(),
             'controller_name' => 'PotController',
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/pot/{id}/settings", name="potSettings")
+     */
+    public function potEdit($id,Request $request): Response
+    {
+        $pot = $this->getDoctrine()->getRepository(Pot::class)->find($id);
+        $form = $this->createForm(PotType::class, $pot);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+             $entityManager = $this->getDoctrine()->getManager();
+             $entityManager->persist($pot);
+             $entityManager->flush();
+        }
+
+
+
+        return $this->render('pot/settings.html.twig', [
+            "Pot" => $pot,
+            'controller_name' => 'PotController',
+            "form" => $form->createView()
         ]);
     }
 }
