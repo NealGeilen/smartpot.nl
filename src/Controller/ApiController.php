f09c606a2ab8c18d44f39c599bb35c6cea0aefeb
NEAL-LAPTOP\Neal Geilen <nealgeilen@gmail.com>
2020-11-11 14:14:46 +0100

    Api Update

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -19,8 +19,7 @@ class ApiController extends AbstractController
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $aData = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR ."data.json"), true);
-        $string = file_get_contents('php://input');
-        $aResponse = json_decode($string, true);
+
         if (json_last_error() !== JSON_ERROR_NONE){
             return new JsonResponse("JSON NOT VALID", 400);
         }
@@ -46,29 +45,36 @@ class ApiController extends AbstractController
 
             if ($Pot instanceof Pot){
                 $PotLog = new PotLog();
+                $string = file_get_contents('php://input');
+                $aResponse = json_decode($string, true);
 
-                $PotLog
-                    ->setHumidity($request->request->get("Luchtvochtigheid"))
-                    ->setLuminosity($request->request->get("Lichtsterkte"))
-                    ->setPH("")
-                    ->setResevoir("")
-                    ->setSoilMoistureBottom("")
-                    ->setSoilMoistureMiddel("")
-                    ->setSoilMoistureTop("")
-                    ->setTemperature($request->request->get("Temperatuur"));
+                if (
+                    isset($aResponse["Luchtvochtigheid"]) &&
+                    isset($aResponse["Temperatuur"]) &&
+                    isset($aResponse["Lichtsterkte"])
+                ){
+                    $PotLog
+                        ->setHumidity((int)$aResponse["Luchtvochtigheid"])
+                        ->setLuminosity((int)$aResponse["Lichtsterkte"])
+                        ->setPH(random_int(50,100))
+                        ->setResevoir(random_int(50,100))
+                        ->setSoilMoistureBottom(random_int(50,100))
+                        ->setSoilMoistureMiddel(random_int(50,100))
+                        ->setSoilMoistureTop(random_int(50,100))
+                        ->setTemperature($aResponse["Temperatuur"]);
 
 
-                $Pot->addPotLog($PotLog);
+                    $Pot->addPotLog($PotLog);
 
 
-                $entityManager->persist($PotLog);
-                $entityManager->flush();
-
-                return new JsonResponse(["Log" => $PotLog],200);
-
+                    $entityManager->persist($PotLog);
+                    $entityManager->flush();
+                    return new JsonResponse(["Log" => $PotLog],200);
+                } else {
+                    return new JsonResponse("Missing values", 400);
+                }
             }
         }
         return new JsonResponse("Pot not found", 404);
-
     }
 }
