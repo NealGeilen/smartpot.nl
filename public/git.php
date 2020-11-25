fda0f039401119c47966f8ddaf80de7bfd31ac11
Neal Geilen <nealgeilen@gmail.com>
2020-11-25 15:19:22 +0100

    Api Update

---
+++ public/git.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This script is for easily deploying updates to Github repos to your local server. It will automatically git clone or
+ * git pull in your repo directory every time an update is pushed to your $BRANCH (configured below).
+ *
+ * INSTRUCTIONS:
+ * 1. Edit the variables below
+ * 2. Upload this script to your server somewhere it can be publicly accessed
+ * 3. Make sure the apache user owns this script (e.g., sudo chown www-data:www-data webhook.php)
+ * 4. (optional) If the repo already exists on the server, make sure the same apache user from step 3 also owns that
+ *    directory (i.e., sudo chown -R www-data:www-data)
+ * 5. Go into your Github Repo > Settings > Service Hooks > WebHook URLs and add the public URL
+ *    (e.g., http://example.com/webhook.php)
+ *
+ **/
+
+// Set Variables
+$LOCAL_ROOT         = __DIR__ . "/../";
+$BRANCH             = "master";
+
+if ( isset($_GET["payload"]) ){
+    if ($_GET["payload"] = "jhdwAJKhdYT#EKJHbwdwau47")
+    echo shell_exec("cd {$LOCAL_ROOT} && git pull");
+}
+?>
